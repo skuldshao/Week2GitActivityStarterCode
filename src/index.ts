c7d106913812411dc2584eaddf2667f1bd6fe6bb
Skuld Shao <77602197+skuldshao@users.noreply.github.com>
2023-09-14 16:20:30 -0400

    implement divide operation

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,15 @@ import { Calculator } from './calculator';
 const calc = new Calculator();
 
 const result_add = calc.add(2, 3);
+
+const result_subtract = calc.subtract(2,3);
+
+const result_multiply = calc.multiply(2,3);
+
+const result_divide = calc.divide(2,3);
+
 console.log(`result_add = ${result_add}`);
+console.log(`result_subtract = ${result_subtract}`);
+console.log(`result_multiply = ${result_multiply}`);
+console.log(`result_divide = ${result_divide}`);
 
